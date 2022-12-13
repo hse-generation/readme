eef2833a0caf0d3eb39901c9d6bdbcd6bccdf0de
GitSanyaHub <cjlil647@gmail.com>
2022-12-13 17:39:20 +0300

    Подправил "Зарегистрироваться" и "Логин", добавил поля в настройках аккаунта и правый верхний значок. Создано приложение books и добавлены модели пользователя, автора, жанров,

---
+++ books/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Books(models.Model):
+    book_name = models.CharField("Название книги", max_length=20, null=True)
+    author_name = models.CharField("Автор", max_length=20, null=True)
+    genre = models.CharField("Жанр", max_length=20, null=True)
+    score = models.IntegerField("Оценка", blank=True, null=True)
+    pages_count = models.IntegerField("Количество страниц", blank=True, null=True)
+    description = models.CharField("Описание", max_length=1000, null=True)
+    pic_link = models.URLField("Ссылка на картинку", max_length = 200, null=True)
+    book_link = models.URLField("Ссылка на книгу", max_length = 200, null=True)
+
+
+
+    def __str__(self):
+        return self.book_name
+
+
+    class Meta:
+        verbose_name = "Книга"
+        verbose_name_plural = "Книги"
+        db_table = "books"
