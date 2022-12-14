1bc0384355128fc825fde0836bc7b459c70884c5
Minhojkhon <minhojkhon@icloud.com>
2022-12-14 22:15:47 +0300

    Added columns description and picture_link, change pic_link

--- genres/models.py
+++ genres/models.py
@@ -5,11 +5,8 @@ from django.db import models
 class Genres(models.Model):
     genre_name = models.CharField("Имя", max_length=100000, null=True)
     creat_date = models.DateField("Дата добавления", null=True)
-    # github_link = models.CharField("Ccылка на github", max_length=200, null=True)
-    # telegram = models.CharField("Ник в телеграм", max_length=200, null=True)
-    # phone_number = models.CharField("Номер телефона", max_length=15, null=True)
-    # specialization = models.CharField("Специализация", max_length=200, null=True)
-
+    description = models.TextField("Описание", null=True)
+    picture_link = models.TextField("Ссылка на картину", max_length=200, null=True)
 
     def __str__(self):
         return self.genre_name
