165f4f5a43f863fcb9213e67a65708f2d5f163bc
Minhojkhon <minhojkhon@icloud.com>
2022-12-15 16:10:42 +0300

    Added author-books page

--- authors/urls.py
+++ authors/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='authors'),
+    path('<int:author_id>', views.get_author_books, name='author_books'),
 ]
--- books/models.py
+++ books/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-
-
+from authors.models import Authors
 # Create your models here.
 
 class Books(models.Model):
     book_name = models.CharField("Название книги",max_length=10000, null=True)
-    author_name = models.CharField("Автор", max_length=10000,null=True)
     genre = models.CharField("Жанр", max_length=10000,null=True)
     score = models.FloatField("Оценка", blank=True, null=True)
     pages_count = models.IntegerField("Количество страниц", blank=True, null=True)
     description = models.TextField("Описание",max_length=10000, null=True)
     pic_link = models.URLField("Ссылка на картинку", max_length=10000, null=True)
     book_link = models.URLField("Ссылка на книгу", max_length=10000, null=True)
+    author_id = models.ForeignKey(Authors, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.book_name
